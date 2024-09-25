8d2c9929c9d07fd011d68116789c371ac2063072
kittipoj11 <poj11@hotmail.com>
2024-09-25 21:07:53 +0700

    edit at 25/09/2024 21:07

--- myjquery.js
+++ myjquery.js
@@ -1,13 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function () {
   // $("H4#hello").html('Hello jQuery');
-  // $(document หรือ "body").on("click", "#btnReload",function () {
-  $("body").on("click", "#btnReload", function () {
+  // $(document หรือ "body").on("click", "#btnReload",function (event) {location.reload();}); หรือ
+  // $(document).on("click", "#btnReload", myFunc); หรือ
+  $("#btnReload").on("click", myFunc);
+  function myFunc(event) {
+    console.log(event.target);
     location.reload();
-  });
-
-  //   $(document หรือ "body").on("click", "#btnHi",function () { หรือ
-  $("#btnHi").click(function (event) {
-    let btn = event.target;
+  }
+  //   $(document หรือ "body").on("click", "#btnHi",function () {}); หรือ
+  $("#btnHi").click(function (e) {
+    console.log(e.target);
+    // let btn = event.target;
 
     $("h4.hello").html("jQuery!!!");
     $("h4#hello").attr("hidden", true);
@@ -24,4 +27,21 @@ $(document).ready(function(){
     $("button#btnHi").attr("disabled", true);
     $("button#btnHi").html("click completed");
   });
+
+  $("div").hover(function () {
+    $(this).css({ 'background-color': 'yellow', 'font-weight': 'bolder' });
+  }, function () {
+    var cssObj = {
+      'background-color': '#ddd',
+      'font-weight': '',
+      'color': 'rgb(0,40,244)',
+      'border': '1px solid red',
+      'border-radius': '50%',
+      'text-align': 'center',
+      'justify-content': 'center',
+    }
+      // < !--$(this).text(''); -->
+        $(this).css(cssObj);
+  });
+
 });
